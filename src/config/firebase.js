1da1fe577aa506078c536c3f899c24649905a8d9
biancalina <bianca.alina1993@gmail.com>
2021-03-07 18:06:51 +0000

    start working with firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC_pjf5oOA585qI_QhjP8Jjrz1ie-cIh64",
+  authDomain: "linkedin-clone-9d7aa.firebaseapp.com",
+  projectId: "linkedin-clone-9d7aa",
+  storageBucket: "linkedin-clone-9d7aa.appspot.com",
+  messagingSenderId: "107628724553",
+  appId: "1:107628724553:web:c615fbe2470eeb1c5a1a98"
+};
