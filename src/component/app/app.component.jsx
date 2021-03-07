af01ea8168deb508cbb3c5530181748df5cf00ec
biancalina <bianca.alina1993@gmail.com>
2021-03-07 17:44:01 +0000

    setup the project

---
+++ src/component/app/app.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TopHeader } from '../top-header'
+import { StyledAppWrapper } from './app.style'
+
+export const App = () => {
+  return (
+    <StyledAppWrapper>
+      <h1>Linkedin</h1>
+      <TopHeader />
+    </StyledAppWrapper>
+  );
+}
+
---
+++ src/component/top-header/top-header.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const TopHeader = () => {
+  return (
+    <div>
+      <nav>
+        <a href="">home</a>
+        <a href="">about me</a>
+        <a href="">contact me</a>
+        <a href="">log in</a>
+      </nav>
+    </div>
+  )
+}
