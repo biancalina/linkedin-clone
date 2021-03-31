512aee45640ab5163ac6c10053de6940bb3a7d3c
biancalina <bianca.alina1993@gmail.com>
2021-03-31 19:26:30 +0100

    add more content and install material-ui

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { TopHeader } from '../top-header'
 import { StyledAppWrapper } from './app.style'
+import SearchIcon from '@material-ui/icons/Search'
 
 export const App = () => {
   return (
     <StyledAppWrapper>
-      <h1>Linkedin</h1>
+      <h1>Linkedin Clone</h1>
+      <div className="header__left">
+        <img src="" alt=""/>
+      </div>
+      <div className="header__right">
+        <SearchIcon />
+        <input type="text"/>
+
+      </div>
       <TopHeader />
     </StyledAppWrapper>
   );
