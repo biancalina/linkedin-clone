d3e3af6cc2f5b86f5c87fde53c1e5a8c7fe6b7ad
biancalina <bianca.alina1993@gmail.com>
2021-04-13 21:50:45 +0100

    :)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import"./App.css";
 function App() {
     return (
         <div className="app">
-            <h1>Let's play with Linkedin</h1>
+            <h1> Linkedin</h1>
             <Header />
         </div>
     );
